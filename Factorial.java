aa80edf4d96223abb789b50758a0d6c0e826a1ac
Rajstar02 <82597145+Rajstar02@users.noreply.github.com>
2021-04-17 11:26:48 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int input = 10;
+				
+		int result = 1;
+		
+		// For multiplication always use 0 as the result 
+		
+		for (int i = 1; i <=10; i++) {
+			
+			result = result*i;
+		}
+			
+			System.out.println(result);
+			
+		
+	}
+		
+	}
