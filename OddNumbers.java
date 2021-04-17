a32b981499708a02a5bbd0a4137006b9fce5be4e
Rajstar02 <82597145+Rajstar02@users.noreply.github.com>
2021-04-17 11:29:11 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Newmethods {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=10; i++) {
+			
+			if (i % 2!=0) {
+				
+				System.out.println(i);
+				
+			}
+			
+			
+		}
+		
+}
+
+}
